5057cdb3d58d349060e3dd1baf7b200f23950c2f
dhayman <dhayman117@gmail.com>
2025-03-30 01:39:50 -0400

    Complete game

--- ModularBoardgame.ino
+++ ModularBoardgame.ino
@@ -6,7 +6,10 @@ enum GameState {
   PLAYERS_SET_COUNT, 
   PLAYERS_SET_COLORS,
   SPACES_SET_COUNT, 
-  PLAYER_TURNS 
+  SPACES_SET_EFFECTS, 
+  SPACES_SET_DESTINATION, 
+  PLAYER_TURNS,
+  GAME_OVER
   };
 GameState gameState = INIT;
 
@@ -33,7 +36,7 @@ struct Player {
 
 const int minPlayers = 1;
 const int maxPlayers = 6; 
-int playerCount = 1;
+int playerCount = minPlayers;
 int currentPlayer = 0;
 
 Player* players;
@@ -44,9 +47,13 @@ CRGB colorChoices[COLOR_COUNT] = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::Yell
 int currentColor = 0;
 
 // SPACE VALUES
-const int minSpaces = 2;
+const int minSpaces = 4;
 const int maxSpaces = 30; 
-int spaceCount = 2;
+int spaceCount = minSpaces;
+
+uint32_t* spacesDests; // array of destination indices when landing on a space. i=j for no action
+int currentSpace = 1;
+int currentDest = 0;
 
 const int diceFaces = 6; 
 
@@ -165,6 +172,7 @@ void decrementColor() {
 void confirmColor() {
   currentPlayer += 1;
   if(currentPlayer >= playerCount){
+    currentPlayer = 0; // reset current player
     displaySpaceCount();
     gameState = SPACES_SET_COUNT;
   } else {
@@ -177,7 +185,7 @@ void displaySpaceCount() {
   lcd.clear();
   lcd.setCursor(0, 0);  //Set cursor to character 2 on line 0
   lcd.print("Enter # Spaces:");
-  lcd.setCursor(4-log10(spaceCount), 1);  //Move cursor to character 2 on line 1
+  lcd.setCursor(4-(int)log10(spaceCount), 1);  //Move cursor to character 2 on line 1
   lcd.print(spaceCount);
   lcd.setCursor(5, 1);  //Move cursor to character 2 on line 1
   lcd.print(" spaces");
@@ -198,9 +206,103 @@ void decrementSpaces() {
 }
 
 void confirmSpaces() {
-  currentPlayer = 0;
-  displayCurrentPlayer();
-  gameState = PLAYER_TURNS;
+  spacesDests = new uint32_t[spaceCount];
+  for (int x = 0; x < spaceCount; x++)
+    spacesDests[x] = x;
+  currentSpace = 1;
+  displayEffect();
+  gameState = SPACES_SET_EFFECTS;
+}
+
+// EFFECT CONFIG
+void renderSpaces(bool callShow = false) {
+  for (int x = 0; x < spaceCount; x++) {
+    if(spacesDests[x] < x)
+      leds[x * (1+maxPlayers)] = CRGB::Red;
+    else if (spacesDests[x] > x)
+      leds[x * (1+maxPlayers)] = CRGB::Green;
+    else
+      leds[x * (1+maxPlayers)] = CRGB::Black;
+  }
+
+  if (callShow)
+    FastLED.show();
+}
+
+void displayEffect() {
+  renderSpaces();
+  leds[currentSpace * (1+maxPlayers)] = CRGB::Blue;
+  FastLED.show();
+
+  lcd.clear();
+  if (currentSpace > 0) {
+    lcd.setCursor(0, 0);  
+    lcd.print("Select Space...");
+  } else {
+    lcd.setCursor(2, 0);  
+    lcd.print("START GAME!");
+  }
+}
+
+void incrementEffects() {
+  if(currentSpace < (spaceCount - 2)) // allow effects up to the penultimate space
+    currentSpace ++;
+
+  displayEffect();
+}
+
+void decrementEffects() {
+  if(currentSpace > 0) // selecting index 0 allows players to exit config
+    currentSpace --;
+
+  displayEffect();
+}
+
+void confirmEffects() {
+  if (currentSpace > 0) {
+    currentDest = currentSpace;
+    displayDestination();
+    gameState = SPACES_SET_DESTINATION;
+  }
+  else{
+    renderSpaces(true);
+    displayCurrentPlayer();
+    gameState = PLAYER_TURNS;
+  }
+}
+
+// DESTINATION CONFIG
+void displayDestination() {
+  renderSpaces();
+  leds[currentDest * (1+maxPlayers)] = CRGB::Yellow;
+  leds[currentSpace * (1+maxPlayers)] = CRGB::Blue;
+  FastLED.show();
+
+  lcd.clear();
+  lcd.setCursor(5, 0);  
+  lcd.print("Select");
+  lcd.setCursor(1, 1);  
+  lcd.print("Destination...");
+}
+
+void incrementDestination() {
+  if(currentDest < (spaceCount - 1)) // allow destinations up to the last space
+    currentDest ++;
+
+  displayDestination();
+}
+
+void decrementDestination() {
+  if(currentDest > 0)
+    currentDest --;
+
+  displayDestination();
+}
+
+void confirmDestination() {
+  spacesDests[currentSpace] = currentDest;
+  displayEffect();
+  gameState = SPACES_SET_EFFECTS;
 }
 
 // GAME LOOP
@@ -214,8 +316,30 @@ void displayCurrentPlayer() {
   lcd.print(currentPlayer + 1);
 }
 
-int diceRoll(){
+int diceRoll() {
   return (esp_random() % diceFaces) + 1;
+}
+
+void movePlayer(int roll) {
+  if(roll > 0) {
+    for(int x = 0; x < roll; x++){
+      delay(500);
+      // get the LED at the position index and player subindex
+      leds[players[currentPlayer].position * (1+maxPlayers) + 1+currentPlayer] = CRGB::Black;
+      players[currentPlayer].position = min((players[currentPlayer].position + 1), spaceCount-1);
+      leds[players[currentPlayer].position * (1+maxPlayers) + 1+currentPlayer] = players[currentPlayer].color;
+      FastLED.show();
+    }
+  } else if (roll < 0) {
+    for(int x = 0; x < abs(roll); x++){
+      delay(500);
+      // get the LED at the position index and player subindex
+      leds[players[currentPlayer].position * (1+maxPlayers) + 1+currentPlayer] = CRGB::Black;
+      players[currentPlayer].position = max((players[currentPlayer].position - 1), 0);
+      leds[players[currentPlayer].position * (1+maxPlayers) + 1+currentPlayer] = players[currentPlayer].color;
+      FastLED.show();
+    }
+  }
 }
 
 void playTurn() {
@@ -238,18 +362,69 @@ void playTurn() {
   lcd.setCursor(8, 1);  //Move cursor to character 2 on line 1
   lcd.print(roll);
 
-  for(int x = 0; x < roll; x++){
+  // Uncomment to add the perfect roll endgame rule
+  // if ((players[currentPlayer].position + roll) > (spaceCount-1)){
+  //   //skip turn
+  //   lcd.clear();
+  //   lcd.setCursor(4, 0);  
+  //   lcd.print("Too high");
+  //   delay(1500);
+  //   return;
+  // }
+
+  movePlayer(roll);
+
+  // if the space the player landed on after rolling is an effect space then trigger the effect
+  if(spacesDests[players[currentPlayer].position] < players[currentPlayer].position) {
+    lcd.clear();
+    lcd.setCursor(3, 0);  //Set cursor to character 2 on line 0
+    lcd.print("Move back:");
+    lcd.setCursor(8, 1);  //Move cursor to character 2 on line 1
+    lcd.print(players[currentPlayer].position - spacesDests[players[currentPlayer].position]);
     delay(500);
-    // get the LED at the position index and player subindex
-    leds[players[currentPlayer].position * (1+maxPlayers) + 1+currentPlayer] = CRGB::Black;
-    players[currentPlayer].position = min((players[currentPlayer].position + 1), spaceCount-1);
-    leds[players[currentPlayer].position * (1+maxPlayers) + 1+currentPlayer] = players[currentPlayer].color;
-    FastLED.show();
+    movePlayer(spacesDests[players[currentPlayer].position] - players[currentPlayer].position); // negative
+
+  } else if (spacesDests[players[currentPlayer].position] > players[currentPlayer].position) {
+    lcd.clear();
+    lcd.setCursor(1, 0);  //Set cursor to character 2 on line 0
+    lcd.print("Move forward:");
+    lcd.setCursor(8, 1);  //Move cursor to character 2 on line 1
+    lcd.print(spacesDests[players[currentPlayer].position] - players[currentPlayer].position);
+    delay(500);
+    movePlayer(spacesDests[players[currentPlayer].position] - players[currentPlayer].position); // positive
   }
 
-  delay(500);
-  currentPlayer = (currentPlayer + 1) % playerCount;
-  displayCurrentPlayer();
+  // game over
+  if(players[currentPlayer].position >= (spaceCount-1)){
+    displayGameOver();
+    gameState = GAME_OVER;
+  // next turn
+  } else {
+    delay(500);
+    currentPlayer = (currentPlayer + 1) % playerCount;
+    displayCurrentPlayer();
+  }
+}
+
+// GAME OVER
+void displayGameOver() {
+  lcd.clear();
+  lcd.setCursor(3, 0);  
+  lcd.print("GAME OVER!");
+  lcd.setCursor(3, 1);  
+  lcd.print("Winner: ");
+  lcd.setCursor(11, 1);  
+  lcd.print(currentPlayer + 1);
+}
+
+void restart() {
+  currentPlayer = 0;
+  for(int x = 0; x < (1+maxPlayers) * maxSpaces; x++){
+    leds[x] = CRGB::Black;
+  }
+  // FastLED.show(); // I'm fine with letting the end state of the last game linger a bit longer
+  displayPlayers();
+  gameState = PLAYERS_SET_COUNT;
 }
 
 void loop() {
@@ -273,8 +448,21 @@ void loop() {
     updateButton(minusButton, decrementSpaces, true);
     updateButton(enterButton, confirmSpaces, false);
     break;
+  case SPACES_SET_EFFECTS:
+    updateButton(plusButton, incrementEffects, true);
+    updateButton(minusButton, decrementEffects, true);
+    updateButton(enterButton, confirmEffects, false);
+    break;
+  case SPACES_SET_DESTINATION:
+    updateButton(plusButton, incrementDestination, true);
+    updateButton(minusButton, decrementDestination, true);
+    updateButton(enterButton, confirmDestination, false);
+    break;
   case PLAYER_TURNS:
     updateButton(enterButton, playTurn, false);
+    break;
+  case GAME_OVER:
+    updateButton(enterButton, restart, false);
     break;
   }
 
